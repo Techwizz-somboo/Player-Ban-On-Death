d0c74daddbf5d5342472e2892769f1d672178428
Techwizz-somboo <44146804+Techwizz-somboo@users.noreply.github.com>
2024-03-31 21:51:46 -0600

    Added a broadcast message!

--- Player_Ban_On_Death.cpp
+++ Player_Ban_On_Death.cpp
@@ -16,17 +16,19 @@ public:
 Player_Ban_On_Death() : PlayerScript("Player_Ban_On_Death") { }
 
 static void ThreadBan(Player* killed,char* text){
-//return;
 killed->Say(text, (Language)0, nullptr);
-//killed->GiveLevel(1);
-//Ban Player
-std::this_thread::sleep_for (std::chrono::seconds(5));
 std::string name = killed->GetName();
+std::string deathMessage = "Attention everyone: " + name + " has died! May they rest in peace!";
+sWorld->SendWorldText(LANG_AUTO_BROADCAST, deathMessage.c_str());
+
+//Ban Player
+std::this_thread::sleep_for (std::chrono::seconds(10));
 char* durationStr = (char*)"-1";
 char* reasonStr = (char*)"Died";
 std::string author = "LevelReset";
 sWorld->BanCharacter(name, durationStr, reasonStr, author);
 }
+
 void OnPVPKill(Player* killer, Player* killed) {
 std::thread first (ThreadBan,killed,(char*)"You killed me, blah!");
 first.detach();
@@ -35,19 +37,6 @@ first.detach();
 void OnPlayerKilledByCreature(Creature* killer, Player* killed) {
 std::thread first (ThreadBan,killed,(char*)"It was good living, blah!");
 first.detach();
-/*
-killed->Say((char*)"It was good living, blah!", (Language)0, nullptr);
-SetTimer(NULL, 0, 2000, &TimerProc);
-Timer t = Timer();
-t.setTimeout([&]() {
-std::string name = killed->GetName();
-char* durationStr = (char*)"-1";
-char* reasonStr = (char*)"Died";
-std::string author = "LevelReset";
-sWorld->BanCharacter(name, durationStr, reasonStr, author);
-    t.stop();
-}, 5000);
-*/
 }
 };
 
